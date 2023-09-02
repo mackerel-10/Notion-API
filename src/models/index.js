5c61bf3f0e3ec946b8dc5ae4f9efdf5d1aaee8fd
Sujin Jeon <mackerel0106@gmail.com>
2023-09-03 00:06:30 +0900

    Feat: 환경변수 처리
    mysql 관련 세팅들 환경변수 처리

--- src/models/index.js
+++ src/models/index.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-  host: 'localhost',
-  port: 3307,
-  user: 'root',
-  password: '',
-  database: 'notion',
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWD,
+  database: process.env.MYSQL_DATABASE,
 });
 
 db.on('connection', () => console.log('MySQL connected!'));
