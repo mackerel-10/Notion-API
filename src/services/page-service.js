4ad87fba52992f4e1d8151829f3f63683573dcc8
Sujin Jeon <mackerel0106@gmail.com>
2023-09-03 00:00:48 +0900

    Feat: breadcrumbs 생성
    배열과 prototype method인 reverse를 이용해 구현

--- src/services/page-service.js
+++ src/services/page-service.js
@@ -1,30 +1,23 @@
 const pageModel = require('../models/page-model');
 
-class ListNode {
-  constructor(val, next) {
-    this.val = val === undefined ? 0 : val;
-    this.next = next === undefined ? null : next;
-  }
-}
-
 async function getNotionPage(req, res) {
   try {
     const { id } = req.params;
-    let breadcrumbs, title, path;
+    let breadcrumbs = [];
+    let path;
 
-    // parent_page가 null일때까지 반복 breadcrumbs head에 삽입
-    // Linked List 활용해서 앞에 삽입
+    // parent_page가 null일때까지 반복
     let targetId = id;
     while (targetId !== null) {
       path = await pageModel.selectParentPage(targetId);
-      console.log(path);
-      if (targetId === id) {
-        title = path.title;
-      }
+      breadcrumbs.push(path.title);
       targetId = path['parent_page'];
     }
 
-    // parent_page가 id인 로우 검색
+    breadcrumbs.reverse();
+    // console.log('breadcrumbs', breadcrumbs);
+
+    // parent_page = id인 row SELECT
     let subPages = await pageModel.selectSubPages(id);
     for (let i = 0; i < subPages.length; i++) {
       subPages[i] = subPages[i].title;
@@ -32,9 +25,9 @@ async function getNotionPage(req, res) {
 
     return res.status(200).json({
       pageId: id,
-      title,
+      title: breadcrumbs[breadcrumbs.length - 1],
       subPages, // []
-      // breadcrumbs, // ["A", "B", "C"]
+      breadcrumbs, // ["A", "B", "C"]
     });
   } catch (error) {
     console.error(error);
